ecebff38ce1baca977a8b62afcc81b25a89c1ef5
Ayesha Azad <ayeshaazad@Ayeshas-MacBook-Pro.local>
2026-02-09 17:57:56 +0500

    Fixing footer and CI and CD

--- seller/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="d-flex pt-3 pb-3 justify-content-center align-items-center text-white">
-    All rights reserved &copy S2402F80B3
-</footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-</html>

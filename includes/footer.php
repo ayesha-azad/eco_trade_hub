b0c86b1c85de716e9bc7720f3704e28a1886a775
Ayesha Azad <ayeshaazad@Ayeshas-MacBook-Pro.local>
2026-02-09 17:59:49 +0500

    Fixing footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer class="d-flex pt-3 pb-3 justify-content-center align-items-center text-white">
-    All rights reserved &copy Ayesha Azad 
+    Ayesha Azad's CI/CD &copy All rights reserved 
 </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
